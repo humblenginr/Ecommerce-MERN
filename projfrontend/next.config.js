eec38d97a767c10b6b53a8e7f2e0624a25788775
suod-NithishKarthik <nithishkarthik01@gmail.com>
2021-02-01 15:16:01 +0530

    Configured env variables

---
+++ projfrontend/next.config.js
@@ -0,0 +1,7 @@
+//these can be accessed in client side
+
+module.exports = {
+    env: {
+      BACKEND: "http://localhost:8000/api/",
+    },
+  };
--- projfrontend/pages/home.js
+++ projfrontend/pages/home.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { baseUrl } from '../Utils/baseUrl';
 
 const home = () => {
+    console.log(baseUrl);
     return (
         <div className="text-white">
             Home page
